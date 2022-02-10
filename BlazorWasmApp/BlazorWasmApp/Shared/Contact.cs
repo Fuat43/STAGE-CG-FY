d54753f525bdbc1c2b664063c9ef0e012252cf72
Fuat Yöndem <56515802+Fuat43@users.noreply.github.com>
2022-02-10 16:53:41 +0100

    wasmApp

---
+++ BlazorWasmApp/BlazorWasmApp/Shared/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmApp.Shared
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact(int contactId, string firstName, string lastName)
+        {
+            ContactId = contactId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Contact(){
+
+        }
+    }
+}
